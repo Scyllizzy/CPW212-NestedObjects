bb4f116d41b050e6c2b3589a44ea3b34d00b5a63
Scyllizzy <babytink2002@hotmail.com>
2020-01-15 14:17:02 -0800

    Created Course class

---
+++ NestedObjects/Course.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedObjects
+{
+    /// <summary>
+    /// Represents a single college course.
+    /// </summary>
+    public class Course
+    {
+        /// <summary>
+        /// The title of the course.
+        /// ex. Programming Fundamentals
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Describes what the course covers/teaches in text.
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// The number of credits earned for completing the class.
+        /// </summary>
+        public byte Credits { get; set; }
+
+        /// <summary>
+        /// The unique course ID assigned to the course by the database. 
+        /// </summary>
+        public int CourseID { get; set; }
+    }
+}
